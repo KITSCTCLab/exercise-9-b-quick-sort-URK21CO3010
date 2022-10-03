ef28819a0b633519751e5e70492d5e985d99fb3a
URK21CO3010 <110015139+URK21CO3010@users.noreply.github.com>
2022-10-04 01:29:27 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,6 +2,19 @@ from typing import List
 
 def quick_sort(data, low, high) -> List[int]:
     # Write code here
+    if low < high:
+        p = partition(data, low, high)
+        quick_sort(data, low, p - 1)
+        quick_sort(data, p + 1, high)
+    return data
+
+def partition(data, low, high):
+    i = low - 1
+    for j in range(low, high):
+        i += 1
+        data[i], data[j] = data[j], data[i]
+    data[i + 1], data[high] = data[high], data[i + 1]
+    
 
 
 input_data = input()
